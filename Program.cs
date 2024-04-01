81d37b014f262ab3c1d022552f501d3692caddef
Murtazoev Alijon <murtazoevalijon@gmail.com>
2024-04-01 04:52:18 +0300

    1st Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_Socket
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Start();
+            while (true)
+                server.Update();
+        }
+    }
+}
